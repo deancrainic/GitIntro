5f0a46172e775071e8735123b90d9daa5ef5a4a7
Dean <dean.crainic@gmail.com>
2022-03-09 15:35:00 +0200

    new message

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World from the second user");
+Console.WriteLine("Hello from the other side");
